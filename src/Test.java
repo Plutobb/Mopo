bb4aded667e1bf88cc62ba6c653caa3cb6d64561
Mopo <1194839756@qq.com>
2021-02-12 23:52:44 +0800

    测试

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,30 @@
 public class Test {
+    public static class MyRunnable implements Runnable{
+        private int id ;
+
+        public MyRunnable(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                System.out.println(i+"---"+ id);
+            }
+        }
+    }
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("1");
+
+
+        MyRunnable myRunnable = new MyRunnable(1);
+        Thread thread = new Thread(myRunnable);
+        thread.start();
+
+        MyRunnable myRunnable2 = new MyRunnable(2);
+        Thread thread1 = new Thread(myRunnable2);
+        thread1.start();
+
     }
 }
