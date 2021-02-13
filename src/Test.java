4a54d76844866ebb4bed66e5b096fa27ce9c0773
Mopo <1194839756@qq.com>
2021-02-13 23:30:50 +0800

    测试

--- src/Test.java
+++ src/Test.java
@@ -13,18 +13,30 @@ public class Test {
             }
         }
     }
+
+    static class MyThread extends Thread{
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                System.out.println(i);
+            }
+        }
+    }
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("1");
 
 
-        MyRunnable myRunnable = new MyRunnable(1);
-        Thread thread = new Thread(myRunnable);
-        thread.start();
+//        MyRunnable myRunnable = new MyRunnable(1);
+//        Thread thread = new Thread(myRunnable);
+//        thread.start();
+//
+//        MyRunnable myRunnable2 = new MyRunnable(2);
+//        Thread thread1 = new Thread(myRunnable2);
+//        thread1.start();
 
-        MyRunnable myRunnable2 = new MyRunnable(2);
-        Thread thread1 = new Thread(myRunnable2);
-        thread1.start();
+        MyThread myThread = new MyThread();
+        myThread.start();
 
     }
 }
