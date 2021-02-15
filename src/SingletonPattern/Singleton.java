7c10d548723fd4953fc217334981ff3c2ba85f9d
Mopo <1194839756@qq.com>
2021-02-16 00:08:36 +0800

    设计模式---单例模式

--- src/SingletonPattern/Singleton.java
+++ src/SingletonPattern/Singleton.java
@@ -5,6 +5,10 @@ public class Singleton {
     private static Singleton instance;
     private Singleton() { }
 
+    void print(){
+        System.out.println("你好！");
+    }
+
     //懒汉式实现，线程不安全版本
     //这种方式是最基本的实现方式，这种实现最大的问题就是不支持多线程。因为没有加锁 synchronized，所以严格意义上它并不算单例模式。
     //这种方式 lazy loading 很明显，不要求线程安全，在多线程不能正常工作。
